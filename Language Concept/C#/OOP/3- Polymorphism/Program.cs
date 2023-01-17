efa3c3463530e9949d65bcc2829cfbda18c0aa87
ifcodey <ifcodey@gmail.com>
2023-01-17 05:13:01 +0200

    add

---
+++ Language Concept/C#/OOP/3- Polymorphism/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+class Program
+{
+    static void Main(String[] args)
+    {
+
+        Employee E = new Employee();
+        E.GetInfo();
+
+    }
+}
+
+public class Person
+{
+    protected string ssn = "444-55-6666";
+    protected string name = "Ahmed Shalash";
+
+    public virtual void GetInfo()
+    {
+        Console.WriteLine("Name: {0}", name);
+        Console.WriteLine("SSN: {0}", ssn);
+    }
+}
+class Employee : Person
+{
+    public string id = "ABC567EFG";
+    public override void GetInfo()
+    {
+        // Calling the base class GetInfo method:
+        base.GetInfo();
+        Console.WriteLine("Employee ID: {0}", id);
+    }
+}
