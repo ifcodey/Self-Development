337e83ab6ebc3bf203cc60b32bf427fafa0761c0
ifcodey <ifcodey@gmail.com>
2023-01-17 22:57:47 +0200

    new

---
+++ Language Concept/C#/OOP/3- Polymorphism/Overloading/1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Xml.Linq;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        Add(1.1F, 1.2F);
+        float x = Add(1, 1.1F);
+        Console.WriteLine("The Return Function Sum {0}", x);
+        Add(1, 1);
+        Add(2, 1, 1);
+    }
+    //OverLoading
+    // 1- change type param.
+    // 2- number of param.
+    // 3- type of param related (out , ref , normal param - value param)
+    public static void Add(int FN, int LN)
+    {
+        Console.WriteLine("Sum ={0} ", FN + LN);
+    }
+
+    public static void Add(float FN, float LN)
+    {
+        Console.WriteLine("Sum ={0} ", FN + LN);
+    }
+
+    public static float Add(int FN, float LN)
+    {
+        return FN + LN;
+    }
+
+    public static void Add(int FN, int SN, int TN)
+    {
+        Console.WriteLine("Sum ={0} ", FN + SN + TN);
+    }
+}
---
+++ Language Concept/C#/OOP/4- Encapsulation/1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Xml.Linq;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        Student student = new Student();
+        student.setID(1);
+        student.setName("test");
+        student.setEmail("test@gmail.com");
+        student.setPhone("0598527415");
+
+        Console.WriteLine("{0}\n{1}\n{2}\n{3}", student.getId(), student.getName(), student.getEmail(), student.getPhone());
+
+
+    }
+
+
+}
+
+public class Student
+{
+    private int _id;
+    private string _name;
+    private string _email;
+    private string _phone;
+
+    //Setter.
+    public void setID(int id)
+    {
+        if (id <= 0)
+        {
+            throw new Exception("Student id should be greater than zero");
+        }
+        this._id = id;
+    }
+    public void setName(string name)
+    {
+        if (string.IsNullOrEmpty(name))
+        {
+            throw new Exception("Student name should be vaild not empty");
+        }
+        this._name = name;
+    }
+    public void setEmail(string email)
+    {
+        if (string.IsNullOrEmpty(email))
+        {
+            throw new Exception("Student email should be vaild");
+        }
+        this._email = email;
+    }
+    public void setPhone(string phone)
+    {
+        this._phone = phone;
+    }
+
+    //Getter.
+    public int getId()
+    {
+        return this._id;
+    }
+    public string getName()
+    {
+        return this._name;
+    }
+    public string getEmail()
+    {
+        return this._email;
+
+    }
+    public string getPhone()
+    {
+        return this._phone;
+    }
+
+}
